43aed01deb7c7168ebec2a5035bd3c016363e543
Tim Boden <tim@keencore.com>
2018-05-22 18:39:29 +0200

    test

--- tests/mega2560_rgb/mega2560_rgb.ino
+++ tests/mega2560_rgb/mega2560_rgb.ino
@@ -1,8 +1,8 @@
 /* PWM Outputs */
 
-const uint8_t Red = 9;
-const uint8_t Green = 10;
-const uint8_t Blue = 11;
+static const uint8_t Red = 9;
+static const uint8_t Green = 10;
+static const uint8_t Blue = 11;
 
 static const uint8_t Pins[] = { Red, Green, Blue };
 
@@ -12,9 +12,9 @@ void setup()
 	pinMode(Green, OUTPUT);
 	pinMode(Blue, OUTPUT);
 
-	analogWrite( Red, 0 );
-	analogWrite( Green, 0 );
-	analogWrite( Blue, 0 );
+	analogWrite( Red, 255 );
+	analogWrite( Green, 255 );
+	//analogWrite( Blue, 255 );
 }
 
 // Sets colour Red=0 Green=1 Blue=2 to specified intensity 0 (off) to 255 (max)
@@ -28,7 +28,7 @@ void loop()
     /*for (int i=0; i <= 255; i++)
     {
       SetColour(0, i );
-      delay(25);
+      delay(25 );
     }*/
 	
   /*for (int colour=0; colour < 3; colour++)

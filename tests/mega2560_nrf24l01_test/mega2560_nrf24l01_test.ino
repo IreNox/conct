f6835c427d56fdf67f276bd9891d48d1f559f2a5
Tim Boden <mail@timboden.de>
2019-02-03 00:08:27 +0100

    tests

--- tests/mega2560_nrf24l01_test/mega2560_nrf24l01_test.ino
+++ tests/mega2560_nrf24l01_test/mega2560_nrf24l01_test.ino
@@ -3,15 +3,19 @@
 #include <RF24.h>
 RF24 radio(7, 8); // CE, CSN
 const byte address[6] = "00001";
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   radio.begin();
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
 }
-void loop() {
-  if (radio.available()) {
+
+void loop()
+{
+  if ( radio.available() )
+  {
     char text[32] = "";
     radio.read(&text, sizeof(text));
     Serial.println(text);

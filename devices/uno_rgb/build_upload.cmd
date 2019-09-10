4966bae29d296c1f07b5c648ce8777e3aaf4192b
Tim Boden <mail@timboden.de>
2019-09-10 23:18:13 +0200

    fix vs code config

---
+++ devices/uno_rgb/build_upload.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+call %~dp0build.cmd
+
+SET ARDUINO_AVR_DUDE=%ARDUINO_HOME%\hardware\tools\avr\bin\avrdude.exe
+
+cd build
+%ARDUINO_AVR_DUDE%  -C%ARDUINO_HOME%\hardware\tools\avr\etc\avrdude.conf -v -patmega328p -carduino -PCOM3 -D -Uflash:w:%ARDUINO_SKETCH%.hex:i 

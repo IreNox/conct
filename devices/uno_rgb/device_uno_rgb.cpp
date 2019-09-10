a525a1118b437204cca7d6fe944796f884065233
Tim Boden <mail@timboden.de>
2019-09-10 23:47:33 +0200

    fix io code

--- devices/uno_rgb/device_uno_rgb.cpp
+++ devices/uno_rgb/device_uno_rgb.cpp
@@ -4,19 +4,22 @@
 
 namespace conct
 {
-	static const uint8 Red = 3;
-	static const uint8 Green = 5;
-	static const uint8 Blue = 6;
+	static const uint8 Red = 6;
+	static const uint8 Green = 9;
+	static const uint8 Blue = 5;
+	static const uint8 Power = 2;
 
 	void Deviceuno_rgb::setup()
 	{
 		pinMode( Red, OUTPUT );
 		pinMode( Green, OUTPUT );
 		pinMode( Blue, OUTPUT );
+		pinMode( Power, OUTPUT );
 
 		analogWrite( Red, 0 );
 		analogWrite( Green, 0 );
 		analogWrite( Blue, 0 );
+		digitalWrite( Power, LOW );
 	}
 
 	void Deviceuno_rgb::loop()
@@ -30,6 +33,7 @@ namespace conct
 		analogWrite( Red, s_light.getRed() / 257 );
 		analogWrite( Green, s_light.getGreen() / 257 );
 		analogWrite( Blue, s_light.getBlue() / 257 );
+		digitalWrite( Power, s_light.getOn() ? HIGH : LOW );
 
 		delay( 10 );
 	}

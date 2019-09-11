264bc1aa381a790e9740f330be480662706d6e17
Tim Boden <mail@timboden.de>
2019-09-11 21:21:56 +0200

    cancel build on error

--- devices/uno_rgb/build.cmd
+++ devices/uno_rgb/build.cmd
@@ -6,3 +6,4 @@ SET PATH= %PATH%;%ARDUINO_HOME%
 
 cd /d %~dp0
 arduino-builder.exe -compile -hardware %ARDUINO_HOME%/hardware -tools %ARDUINO_HOME%/hardware/tools -tools %ARDUINO_HOME%/tools-builder -libraries ../../libraries -libraries ../../third_party -fqbn arduino:avr:uno -build-path ./build %ARDUINO_SKETCH%
+if %ERRORLEVEL% GEQ 1 exit 1

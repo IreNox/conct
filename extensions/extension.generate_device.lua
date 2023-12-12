9cf217935d8f9fb252c18f1078cdbe362afc3937
Tim Boden <mail@timboden.de>
2023-12-12 02:19:46 +0100

    fix build extension

--- extensions/extension.generate_device.lua
+++ extensions/extension.generate_device.lua
@@ -4,20 +4,20 @@ ProjectExtensions:add_new_hook( function( project )
 end )
 
 ProjectExtensions:add_pre_finalize_hook( function( solution, project )
-	local exe_name = iff( tiki.platform == Platforms.Windows, ".exe", "" )
+	local exe_name = iff( tiki.host_platform == Platforms.Windows, ".exe", "" )
 
 	for _, filename in pairs( project.device_files ) do
 		local definition_path = path.join( project.module.config.base_path, filename )
 	
 		local step_data = {
-			tool_path = path.join( tiki.root_path, "apps/generator/build/Release_x64/generator" .. exe_name ),
+			tool_path = path.join( tiki.root_path, "apps/generator/build/x64/Release/generator" .. exe_name ),
 			definition_path = definition_path
 		}
 		
 		project:add_pre_build_step( "generate_device", step_data )
 		project:execute_build_step( "generate_device", step_data )
 
-		local output_path = path.getdirectory( definition_path )
+		local output_path = path.getdirectory( filename )
 		project:add_files( output_path .. "/gen/*.*" )
 	end
 end )
